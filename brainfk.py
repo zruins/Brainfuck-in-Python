6661ce7196b3fb9a6d0f8a8633288e6f4685f241
zruins <93773775+zruins@users.noreply.github.com>
2022-05-16 12:37:34 -0400

    Add file support

--- brainfk.py
+++ brainfk.py
@@ -8,7 +8,7 @@ def main(arg):
 	
 	pointer = 0
 
-	write("a=[]\nptr=0\niter=0\nwhile iter<64:\n\ta.append(0)\n\titer+=1\n")
+	write("outputstr=\"\"\na=[]\nptr=0\niter=0\nwhile iter<64:\n\ta.append(0)\n\titer+=1\n")
 	while pointer < len(prompt):
 		loopCheck = ("\t" if inLoop else "")
 		if prompt[pointer] == "+":
@@ -27,22 +27,30 @@ def main(arg):
 		elif prompt[pointer] == "]":
 			inLoop = False
 		elif prompt[pointer] == ".":
-			write(loopCheck + "print(chr(a[ptr]))\n")
+			write(loopCheck + "outputstr+=str(chr(a[ptr]))\n")
 		elif prompt[pointer] == ",":
 			write(loopCheck + "a[ptr]=ord(input()[0])\n")
 		pointer += 1
+	write("print(outputstr)")
 	output.close()
 
 	
 
 if __name__ == "__main__":
-	output = open("out.py", "w")
-	print("Python Brainf**k Compiler v1.0.0, say \"quit\" to exit")
+	output = open("out", "w")
+	print("Python Brainf**k Compiler v1.1.1, say \"quit\" to exit")
 	while True:
 		i = input("$ ")
 		if i != "quit":
-			main(i)
-			system("python3 out.py")
+			if i[0] != "*":
+				main(i)
+			else:
+				lis = list(i)
+				lis.remove("*")
+				f = open("".join(lis))
+				main(f.read())
+				f.close()
+			system("python3 out")
 		else:
 			break
-	system("rm out.py")
+	system("rm out")
